df8f368f61037bbcd24d592c42179ee6f4e89ff4
Paul Jaworski <paulmattjaws@gmail.com>
2020-02-22 13:22:19 -0800

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+    'plugin:prettier/recommended',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/jsx-props-no-spreading': 'off',
+  },
+};
---
+++ src/utils/theme.js
@@ -0,0 +1,47 @@
+export default {
+  breakpoints: ['40em', '52em', '64em'],
+  fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 80],
+  colors: {
+    primary: '#171717',
+    text: '#333',
+    gray: '#666',
+    grayLight: '#C2C2C2',
+    grayLighter: '#E5E5E5',
+    offWhite: '#F8F8F8',
+    white: '#fff',
+    purple: '#6E1EFF',
+    purpleLight: '#D3BBFF',
+    purpleLighter: '#F8F3FF',
+    green: '#0EDA83',
+    yellow: '#F1AF1B',
+    red: '#F7625A',
+  },
+  space: [0, 4, 8, 16, 32, 64, 128, 256],
+  fonts: {
+    body: 'acumin-pro, sans-serif',
+    heading: 'acumin-pro, sans-serif',
+    monospace: 'Menlo, monospace',
+  },
+  fontWeights: {
+    body: 400,
+    heading: 700,
+    bold: 700,
+  },
+  lineHeights: {
+    body: 1.5,
+    heading: 1.25,
+  },
+  shadows: {
+    small: '0 0 4px rgba(0, 0, 0, .125)',
+    large: '0 0 24px rgba(0, 0, 0, .125)',
+  },
+  variants: {},
+  text: {},
+  buttons: {
+    primary: {
+      color: 'white',
+      bg: 'primary',
+      hoverBg: 'purple',
+    },
+  },
+};
